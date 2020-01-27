190c0d273ccdcf472aca8d9eab84cbd5699fd559
Adriana da Mata <adriana.damata.prof@gmail.com>
2020-01-27 20:15:25 -0300

    Exercício sobre Tratamento de Exceções

---
+++ Exercicio_Excecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exercicio_Excecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
